26ebde3b0b1254de9e8fa17d4d1313567328c7d7
shokheralna <rahatfariautube@gmail.com>
2026-09-02 15:38:08 -0400

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1179,7 +1179,7 @@ window.SHOKHER_ALNA_PRODUCTS = [
     "category": "bags",
     "folder": "assets/products/bags/B030-Guzrati-Metal-Bag",
     "price": 60,
-    "status": "available",
+    "status": "active",
     "featured": true,
     "bestSeller": true,
     "newArrival": true,
