6bf1336a603acd76be0c7d02bf4e9f94327af902
shokheralna <rahatfariautube@gmail.com>
2026-09-02 16:06:28 -0400

    Update products.js

--- data/products.js
+++ data/products.js
@@ -8,7 +8,7 @@ window.SHOKHER_ALNA_PRODUCTS = [
     "name": "Green & White Chand Tara Set",
     "category": "jewelry",
     "folder": "assets/products/jewelry/J001-Green-White-Chand-Tara-Set",
-    "price": 20,
+    "price": 1,
     "status": "active",
     "featured": false,
     "bestSeller": false,
